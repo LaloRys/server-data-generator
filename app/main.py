a9d56ac130e537db40cdab6a00cedfe80da6bea1
DevRys <eduardoreyes55@aragon.unam.mx>
2024-01-12 17:33:51 -0600

    fixed host='0.0.0.0'

--- app/main.py
+++ app/main.py
@@ -172,4 +172,4 @@ async def download(file_path: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=0.0.0.0 ,port=10000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
