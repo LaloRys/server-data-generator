05a3538191174e4a9fe5825eb7103fede6ee02cb
DevRys <eduardoreyes55@aragon.unam.mx>
2024-05-06 21:20:05 -0600

    Create Dockerfile

--- app/main.py
+++ app/main.py
@@ -381,5 +381,5 @@ host = os.getenv("HOST", "127.0.0.1")
 port = int(os.getenv("PORT", 5000))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port, debug=True)
-    # uvicorn.run("main:app", host=host, port=port, reload=True)
+    # app.run(host="0.0.0.0", port=port, debug=True)
+    uvicorn.run("main:app", host=host, port=port, reload=True)
